906cf7dc9d66145ceac7784fbdc6c68e74fc7d54
Manas Pratim Biwas <manaspb2405@gmail.com>
2023-12-21 21:39:16 +0530

    [feat]: hubspot oauth completed

--- assessment/backend/integrations/hubspot.py
+++ assessment/backend/integrations/hubspot.py
@@ -8,6 +8,7 @@ import httpx
 import asyncio
 import base64
 import requests
+import hashlib
 from integrations.integration_item import IntegrationItem
 
 from redis_client import add_key_value_redis, get_value_redis, delete_key_redis
@@ -21,75 +22,86 @@ REDIRECT_URI = 'http://localhost:8000/integrations/hubspot/oauth2callback'
 authorization_url = f'https://app.hubspot.com/oauth/authorize?client_id={CLIENT_ID}&redirect_uri=http://localhost:8000/integrations/hubspot/oauth2callback&scope=crm.schemas.contacts.read%20crm.schemas.contacts.write'
 
 async def authorize_hubspot(user_id, org_id):
+
     state_data = {
         'state': secrets.token_urlsafe(32),
         'user_id': user_id,
         'org_id': org_id
     }
-    encoded_state = json.dumps(state_data)
-    auth_url = f'{authorization_url}&state={encoded_state}'
-    await add_key_value_redis(f'hubspot_state:{org_id}:{user_id}', encoded_state, expire=600)
+    encoded_state = base64.urlsafe_b64encode(json.dumps(state_data).encode('utf-8')).decode('utf-8')
+
+    code_verifier = secrets.token_urlsafe(32)
+    m = hashlib.sha256()
+    m.update(code_verifier.encode('utf-8'))
+    code_challenge = base64.urlsafe_b64encode(m.digest()).decode('utf-8').replace('=', '')
+
+    auth_url = f'{authorization_url}&state={encoded_state}&code_challenge={code_challenge}&code_challenge_method=S256'
+    await asyncio.gather(
+        add_key_value_redis(f'hubspot_state:{org_id}:{user_id}', json.dumps(state_data), expire=600),
+        add_key_value_redis(f'hubspot_verifier:{org_id}:{user_id}', code_verifier, expire=600),
+    )
 
     return auth_url
 
 async def oauth2callback_hubspot(request: Request):
-    # if request.query_params.get('error'):
-    #     raise HTTPException(status_code=400, detail=request.query_params.get('error'))
-    # code = request.query_params.get('code')
-    # encoded_state = request.query_params.get('state')
-    # state_data = json.loads(encoded_state)
 
-    # original_state = state_data.get('state')
-    # user_id = state_data.get('user_id')
-    # org_id = state_data.get('org_id')
+    if request.query_params.get('error'):
+        raise HTTPException(status_code=400, detail=request.query_params.get('error_description'))
+    code = request.query_params.get('code')
+    encoded_state = request.query_params.get('state')
+    state_data = json.loads(base64.urlsafe_b64decode(encoded_state).decode('utf-8'))
 
-    # saved_state = await get_value_redis(f'hubspot_state:{org_id}:{user_id}')
+    original_state = state_data.get('state')
+    user_id = state_data.get('user_id')
+    org_id = state_data.get('org_id')
 
-    # if not saved_state or original_state != json.loads(saved_state).get('state'):
-    #     raise HTTPException(status_code=400, detail='State does not match.')
+    saved_state, code_verifier = await asyncio.gather(
+        get_value_redis(f'hubspot_state:{org_id}:{user_id}'),
+        get_value_redis(f'hubspot_verifier:{org_id}:{user_id}'),
+    )
 
-    # async with httpx.AsyncClient() as client:
-    #     response, _ = await asyncio.gather(
-    #         client.post(
-    #             'https://api.hubapi.com/oauth/v1/token',
-    #             json={
-    #                 'grant_type': 'authorization_code',
-    #                 'client_id': CLIENT_ID,
-    #                 'client_secret': CLIENT_SECRET,
-    #                 'redirect_uri': REDIRECT_URI,
-    #                 'code': code,
-    #             }, 
-    #             headers={
-    #                 'Authorization': f'Basic {encoded_client_id_secret}',
-    #                 'Content-Type': 'application/json',
-    #             }
-    #         ),
-    #         delete_key_redis(f'hubspot_state:{org_id}:{user_id}'),
-    #     )
+    if not saved_state or original_state != json.loads(saved_state).get('state'):
+        raise HTTPException(status_code=400, detail='State does not match.')
 
-    # await add_key_value_redis(f'hubspot_credentials:{org_id}:{user_id}', json.dumps(response.json()), expire=600)
+    async with httpx.AsyncClient() as client:
+        response, _, _ = await asyncio.gather(
+            client.post(
+                'https://api.hubapi.com/oauth/v1/token',
+                data={
+                    'grant_type': 'authorization_code',
+                    'client_id': CLIENT_ID,
+                    'client_secret': CLIENT_SECRET,
+                    'redirect_uri': REDIRECT_URI,
+                    'code': code,
+                }, 
+                headers={
+                    'Authorization': f'Basic {encoded_client_id_secret}',
+                    'Content-Type': 'application/x-www-form-urlencoded',
+                }
+            ),
+            delete_key_redis(f'hubspot_state:{org_id}:{user_id}'),
+            delete_key_redis(f'hubspot_verifier:{org_id}:{user_id}'),
+        )
+
+    await add_key_value_redis(f'hubspot_credentials:{org_id}:{user_id}', json.dumps(response.json()), expire=600)
     
-    # close_window_script = """
-    # <html>
-    #     <script>
-    #         window.close();
-    #     </script>
-    # </html>
-    # """
-    # return HTMLResponse(content=close_window_script)
-    pass
+    close_window_script = """
+    <html>
+        <script>
+            window.close();
+        </script>
+    </html>
+    """
+    return HTMLResponse(content=close_window_script)
 
 async def get_hubspot_credentials(user_id, org_id):
-    # credentials = await get_value_redis(f'hubspot_credentials:{org_id}:{user_id}')
-    # if not credentials:
-    #     raise HTTPException(status_code=400, detail='No credentials found.')
-    # credentials = json.loads(credentials)
-    # if not credentials:
-    #     raise HTTPException(status_code=400, detail='No credentials found.')
-    # await delete_key_redis(f'hubspot_credentials:{org_id}:{user_id}')
+    credentials = await get_value_redis(f'hubspot_credentials:{org_id}:{user_id}')
+    if not credentials:
+        raise HTTPException(status_code=400, detail='No credentials found.')
+    credentials = json.loads(credentials)
+    await delete_key_redis(f'hubspot_credentials:{org_id}:{user_id}')
 
-    # return credentials
-    pass
+    return credentials
 
 async def create_integration_item_metadata_object(response_json):
     # TODO
